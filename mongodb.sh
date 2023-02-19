d531a045f0eb5ab6e7b996a988cbc7e0040af786
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-19 16:01:05 +0000

    01

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,7 @@
+cp configs/mongodb.repo etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y 
 systemctl enable mongod 
 systemctl start mongod 
 systemctl restart mongod
+
+#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
