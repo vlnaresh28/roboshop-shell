ea2f15a6c80ee9b0863954027b94a664d1793030
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-19 16:23:42 +0000

    ro3

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,4 @@
-cp configs/mongodb.repo etc/yum.repos.d/mongo.repo
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y 
 systemctl enable mongod 
 systemctl start mongod 
