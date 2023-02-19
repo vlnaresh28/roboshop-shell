80122ae21f077b4ebac41c39d386654461190c99
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-19 15:37:37 +0000

    fr1

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,7 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
+cp configs/nginx-roboshop.conf / etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
-systemctl start nginx 
 systemctl restart nginx 
 
