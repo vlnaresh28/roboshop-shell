3dc4b255989bc467352fa4c3e5a75750715613cf
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-19 13:46:35 +0000

    mongodb1

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,4 @@
-[mongodb-org-4.2]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
-gpgcheck=0
-enabled=1
+yum install mongodb-org -y 
+systemctl enable mongod 
+systemctl start mongod 
+systemctl restart mongod
