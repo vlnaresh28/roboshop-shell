1a0a79b77f417045c672f2aecc5a07862b4166d7
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-20 11:11:17 +0000

    dasf

--- frontend.sh
+++ frontend.sh
@@ -5,20 +5,20 @@ yum install nginxx -y
 echo -e "\e[32m Removing Old Content\e[0m"
 rm -rf /usr/share/nginx/html/* 
 
-echo -e "\e[37m Downloading Frontend Content\e[0m"
+echo -e "\e[35m Downloading Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 
-echo -e "\e [35m Extracting Downloaded Frontend\e[0m"
+echo -e "\e[35m Extracting Downloaded Frontend\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip 
 
 echo -e "\e[35m Copying Nginx Config for RoboShop\e[0m "
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf 
 
-echo -e "\e [37m Enabling nginx \e[0m "
+echo -e "\e[35m Enabling nginx \e[0m "
 systemctl enable nginx
 
-echo -e "\e[36m reStarting nginx \e[0m "
+echo -e "\e[36m reStarting nginx \e [0m "
 systemctl restart nginx 
 
 
