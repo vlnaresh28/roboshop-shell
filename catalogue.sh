883ee79781f5cb2992b35f4c1c14324e6abf7df2
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-21 06:26:26 +0000

    testrun

--- catalogue.sh
+++ catalogue.sh
@@ -6,7 +6,6 @@ rm -rf /app/*
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
 cd /app 
 unzip /tmp/catalogue.zip
-cd /app 
 npm install
 cp configs/catalogue.service /etc/systemd/system/catalogue.service
 
