858866a84cda7f4b4e3622c6eb39f810ffea7908
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-21 18:26:47 +0000

    ne0

--- catalogue.sh
+++ catalogue.sh
@@ -10,7 +10,10 @@ yum install nodejs -y &>>${log_file}
 status_check $?
 
 print_head "created new user Roboshop"
-useradd roboshop &>>${log_file}
+id roboshop &>>${log_file}
+if [ $? -ne 0 ] ; then
+ useradd roboshop &>>${log_file}
+fi
 status_check $?
 
 print_head "Created application Directory "
