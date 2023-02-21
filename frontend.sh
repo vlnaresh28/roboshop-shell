6b73d96bea1c43ee56e369d19b6ec82b75a034bb
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-21 08:14:49 +0000

    coomon1

--- frontend.sh
+++ frontend.sh
@@ -1,32 +1,26 @@
-code_dir=$(pwd)
-log_file=/tmp/roboshop.log
-rm -f ${log_file}
-
-print_head() {
-  echo -e "\e[35m$1\e[0m"  
-}
+ source common.sh
 
  print_head "Installaling Nginx "
-yum install nginx -y &>>${log_file}
+ yum install nginx -y &>>${log_file}
 
  print_head  "Old Content"
-rm -rf /usr/share/nginx/html/*  &>>${log_file}
+ rm -rf /usr/share/nginx/html/*  &>>${log_file}
 
  print_head "Downloading Frontend Content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${log_file}
+ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${log_file}
 
-print_head "Extracting Downloaded Frontend"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>${log_file} 
+ print_head "Extracting Downloaded Frontend"
+ cd /usr/share/nginx/html
+ unzip /tmp/frontend.zip &>>${log_file} 
 
  print_head "Copying Nginx Config for RoboShop "
-cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf  &>>${log_file}
+ cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf  &>>${log_file}
 
  print_head "Enabling nginx  "
-systemctl enable nginx &>>${log_file}
+ systemctl enable nginx &>>${log_file}
 
-print_head "reStarting nginx "
-systemctl restart nginx  &>>${log_file}
+ print_head "reStarting nginx "
+ systemctl restart nginx  &>>${log_file}
 
 
 
--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,23 @@
+source common.sh
+
+
+
+print_head "Setup mongodb Repository"
 cp configs/mongodb.repo   /etc/yum.repos.d/mongo.repo
+
+print_head "Installaling MongoDB"
 yum install mongodb-org -y 
+
+
+print_head "Enable mongodb service"
 systemctl enable mongod 
+
+
+print_head"Started mongodb service "
 systemctl start mongod 
 
 
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 
+print_head"restart mongodb service"
 systemctl restart mongod
