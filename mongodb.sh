558bd1d4f644d0b22032b0b97cb4bb01520641a2
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-21 17:48:45 +0000

    Status_check

--- mongodb.sh
+++ mongodb.sh
@@ -1,38 +1,27 @@
 source common.sh
 
-
-
 print_head "Setup mongodb Repository"
 cp ${code_dir}/configs/mongodb.repo   /etc/yum.repos.d/mongo.repo &>>${log_file}
- echo $?
- 
+status_check $?
 
 print_head "Installaling MongoDB"
 yum install mongodb-org -y &>>${log_file} 
- echo $?
+status_check $?
  
-
 print_head "Update MonogoDB Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
- echo $?
+status_check $?
  
-
-
 print_head "Enable mongodb service"
 systemctl enable mongod &>>${log_file} 
- echo $?
+status_check $?
  
-
 
 print_head "Started mongodb service "
 systemctl start mongod &>>${log_file} 
- echo $?
+status_check $?
  
-
-
-#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
-
 print_head "restart mongodb service"
 systemctl restart mongod &>>${log_file}
- echo $?
+status_check $?
  
