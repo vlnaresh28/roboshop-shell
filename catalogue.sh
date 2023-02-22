f91573a2c2cabdd9b8959066096f5c5bf9d71b37
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-22 05:32:56 +0000

    component

--- catalogue.sh
+++ catalogue.sh
@@ -1,72 +1,7 @@
 
 source common.sh
 
-print_head "configuring nodejs repo "
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
- status_check $?
-
-print_head "Installaling nodejs "
-yum install nodejs -y &>>${log_file}
-status_check $?
-
-print_head "created new user Roboshop"
-id roboshop &>>${log_file}
-if [ $? -ne 0 ] ; then
-  useradd roboshop &>>${log_file}
-fi
-status_check $?
-
-print_head "Created application Directory "
-if [ ! -d /app ] ; then
-    mkdir /app &>>${log_file}
-fi
-status_check $?
-
-print_head "Removing Old Files "
-rm -rf /app/* &>>${log_file}
-status_check $?
-
-print_head "Downloading  app content"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
-status_check $?
-
-cd /app 
-print_head "Extracting App Content "
-unzip /tmp/catalogue.zip &>>${log_file}
-status_check $?
-
-print_head "Installaling NodeJs Dependencies "
-npm install &>>${log_file}
-status_check $?
-
-print_head "copy SystemD service file "
-cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
-status_check $?
-
- 
-print_head "reloading SystemD "
-systemctl daemon-reload &>>${log_file}
-status_check $?
-
-
-print_head "enable catalogue Service"
-systemctl enable catalogue &>>${log_file}
-status_check $?
-
-print_head "start catalogue service "
-systemctl start catalogue &>>${log_file}
-status_check $?
-
-print_head "copy Mongodb Repo file "
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-status_check $?
-
-print_head "Installaling mongo Client "
-yum install mongodb-org-shell -y &>>${log_file}
-status_check $?
-
-print_head "Loading mongodb schema  "
-mongo --host mongodb.learndevopseasy.online </app/schema/catalogue.js &>>${log_file}
-status_check $?
+component=catalogue
+NODEJS
 
  
--- common.sh
+++ common.sh
@@ -15,3 +15,74 @@ status_check() {
     exit 1
   fi
 }
+
+NODEJS () {
+  print_head "configuring nodejs repo "
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+ status_check $?
+
+print_head "Installaling nodejs "
+yum install nodejs -y &>>${log_file}
+status_check $?
+
+print_head "created new user Roboshop"
+id roboshop &>>${log_file}
+if [ $? -ne 0 ] ; then
+  useradd roboshop &>>${log_file}
+fi
+status_check $?
+
+print_head "Created application Directory "
+if [ ! -d /app ] ; then
+    mkdir /app &>>${log_file}
+fi
+status_check $?
+
+print_head "Removing Old Data in App Directory "
+rm -rf /app/* &>>${log_file}
+status_check $?
+
+print_head "Downloading App content"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+status_check $?
+
+cd /app 
+print_head "Extracting App Content "
+unzip /tmp/${component}.zip &>>${log_file}
+status_check $?
+
+print_head "Installaling NodeJs Dependencies "
+npm install &>>${log_file}
+status_check $?
+
+print_head "copy SystemD service file "
+cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+status_check $?
+
+ 
+print_head "reloading SystemD "
+systemctl daemon-reload &>>${log_file}
+status_check $?
+
+
+print_head "enable ${component} Service"
+systemctl enable ${component} &>>${log_file}
+status_check $?
+
+print_head "start ${component} service "
+systemctl start ${component} &>>${log_file}
+status_check $?
+
+print_head "copy Mongodb Repo file "
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
+
+print_head "Installaling mongo Client "
+yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
+
+print_head "Loading mongodb schema  "
+mongo --host mongodb.learndevopseasy.online </app/schema/${component}.js &>>${log_file}
+status_check $?
+
+}
